f003f2d637e282cb391c2bbb9e2f150480e2a457
Sv <ranganathan.sv@accenture.com>
2023-02-01 16:40:06 +0530

    changed jwtValidation

--- src/middlewares/validateJwt.middleware.ts
+++ src/middlewares/validateJwt.middleware.ts
@@ -7,11 +7,12 @@ export class ValidateJwtMiddleware implements NestMiddleware {
   constructor() {}
 
   use(req: Request, res: Response, next: NextFunction) {
-    const cookie = req.cookies['chat-mate-jwt']
-    if(!cookie) throw new UnauthorizedException("Request is missing api-token, Please login again to continue!")
-    const data = jwt.verify(cookie, jwtConstants.secret)
-    if(!data) throw new UnauthorizedException("Invalid api-token in request, Please login again to continue!")
-    req.headers['id'] = data['id']
+    // const cookie = req.cookies['chat-mate-jwt']
+    // if(!cookie) throw new UnauthorizedException("Request is missing api-token, Please login again to continue!")
+    // const data = jwt.verify(cookie, jwtConstants.secret)
+    // if(!data) throw new UnauthorizedException("Invalid api-token in request, Please login again to continue!")
+    // req.headers['id'] = data['id']
+    if(!req.headers['id']) throw new UnauthorizedException("No valid ID in request headers, please logout and login again!")
     next();
   }
 }
